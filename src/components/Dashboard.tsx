ed3677bf26929c1aa8535d1dbe6ecf256ea46677
Faheem Ul Hassan Aziz <seebiz.gpt0623.dev56@gmail.com>
2024-03-18 15:06:38 +0500

    did the css with the help of tailwind

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -31,19 +31,26 @@ function Dashboard() {
         setEditId(index);
     }
 
+    function handleTaskDelete(index: number): void {
+        const updatedTodos = [...todos];
+        updatedTodos.splice(index, 1);
+        setTodos(updatedTodos);
+    }
+
     return (
-        <div>
-            <h1>To do List in TypeScript</h1>
-            <form onSubmit={handleAddTaskFrom}>
-                <input type="text" placeholder="Add a task" value={taskInput} onChange={handleTaskInput} />
-                <button type="submit">Add</button>
+        <div className=" flex flex-col justify-center items-center w-screen h-screen" >
+            <h1 className=" text-[40px] font-bold">To do List in TypeScript</h1>
+            <form onSubmit={handleAddTaskFrom} className=" flex flex-col justify-center items-center w-[300px]">
+                <input type="text" placeholder="Add a task" value={taskInput} onChange={handleTaskInput} className=" bg-gray-500 rounded-lg p-2 mb-5"/>
+                <button type="submit"  className=" bg-green-700 w-[150px] rounded-lg  p-2 mb-5">Add</button>
             </form>
 
             <ul>
                 {todos.map((todo, index) => (
                     <li key={index}>
-                        <span>{todo}</span>
-                        <button onClick={() => handleTaskEdit(index)}>Edit</button>
+                        <span className=" bg-slate-200 p-2 shadow-2xl rounded-md border-2 border-black">{todo}</span>
+                        <button onClick={() => handleTaskEdit(index)}  className=" bg-yellow-700 w-[100px] rounded-lg ml-3 p-2">Edit</button>
+                        <button onClick={() => handleTaskDelete(index)}  className=" bg-red-700 w-[100px] rounded-lg ml-3 p-2">Delete</button>
                     </li>
                 ))}
             </ul>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -35,12 +35,12 @@ function formValidate(){
     }
 
   return (
-    <div>
-        <h1>Login Page</h1>
-        <form onSubmit={handleLoginFromSubmit}>
-        <input type="Email" placeholder="Email" onChange={handleLoginEmail} />
-        <input type="password" placeholder="Password" onChange={handleLoginPassword} />
-        <button type="submit">Login</button>
+    <div className="w-[300px] flex flex-col justify-center items-center m-auto h-[100vh] ">
+        <h1 className=" text-[40px] font-bold">Login Page</h1>
+        <form onSubmit={handleLoginFromSubmit} className=" flex flex-col justify-center items-center ">
+        <input type="Email" placeholder="Email" onChange={handleLoginEmail} className=" bg-gray-500 rounded-lg  p-2 mb-5" />
+        <input type="password" placeholder="Password" onChange={handleLoginPassword} className=" bg-gray-500 rounded-lg  p-2 mb-5"  />
+        <button type="submit"  className=" bg-green-700 w-[150px] rounded-lg  p-2" >Login</button>
         </form>
     </div>
   )
